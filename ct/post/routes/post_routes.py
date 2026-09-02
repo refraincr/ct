63f5a0992c8d76dd5ef50b65a4733cbad057c9cf
refraincr <xinchunzhong4@gmail.com>
2026-09-02 22:11:11 +0800

    分页查询

--- ct/post/routes/post_routes.py
+++ ct/post/routes/post_routes.py
@@ -1,7 +1,8 @@
 from flask import request,Blueprint
 
 from ct.post.services.post_services import (get_all_posts,
-                         publish_post)
+                                            publish_post,
+                                            get_posts_by_user_id_service)
 
 from flask_jwt_extended import jwt_required
 
@@ -10,8 +11,14 @@ bp = Blueprint('post',__name__)
 
 @bp.get('/posts')
 def posts():
-    return get_all_posts()
+    return get_all_posts(request)
 
 @bp.post('/publish')
+@jwt_required()
 def publish():
     return publish_post(request)
+
+@bp.get('/posts/<int:user_id>')
+@jwt_required()
+def get_posts_by_user_id(user_id):
+    return get_posts_by_user_id_service(request,user_id)
--- ct/post/services/post_services.py
+++ ct/post/services/post_services.py
@@ -1,28 +1,55 @@
 from flask.wrappers import Request
-from sqlalchemy import select
+from sqlalchemy import select,func
+
 
 from ct.constants import SUCCESS_CODE, ERROR_CODE
 from ct.extensions import db
 from ct.post.models.post_model import Post
+from ct.user.models.user_model import User
 # 入参
 from ct.post.BO.post_bo import PostPublishBO
 # 响应结构
 from ct.vo import PostResponse, ApiResponse
 
 
-def get_all_posts() -> tuple[ApiResponse, int]:
-    result: list[PostResponse] = []
-    stmt = select(Post)
-    data: list[Post] = db.session.execute(stmt).scalars().all()
-    for i in range(len(data)):
-        user = data[i].user
-        result.append(PostResponse(
-            title=data[i].title,
-            content=data[i].content,
-            create_at=data[i].create_at,
-            username=user.username,
-            avatar=user.avatar
-        ))
+def get_all_posts(request: Request) -> tuple[ApiResponse, int]:
+    try:
+        page = max(int(request.args.get('page',1)),1)
+        page_size = min(max(int(request.args.get('page_size',10)),1),100)
+    except ValueError:
+        return ApiResponse(code=ERROR_CODE,message='分页参数错误').model_dump(),400
+
+    # 获取数据总量
+    total_stmt = select(func.count()).select_from(Post)
+    total: int = db.session.execute(total_stmt).scalar_one()
+
+    # 获取分页数据
+    stmt = (
+        select(Post)
+        .order_by(Post.create_at.desc())
+        .offset((page-1)*page_size)
+        .limit(page_size)
+    )
+    data = db.session.execute(stmt).scalars().all()
+
+    select_result = [
+        PostResponse(
+            title=post.title,
+            content=post.content,
+            username=post.user.username,
+            avatar=post.user.avatar,
+            create_at=post.create_at
+        )
+        for post in data
+    ]
+
+    result = {
+        'result': select_result,
+        'total': total,
+        'page': page,
+        'page_size': page_size,
+        'total_pages': (total + page_size - 1) // page_size
+    }
 
     return ApiResponse(code=SUCCESS_CODE, data=result).model_dump(), 200
 
@@ -40,13 +67,50 @@ def publish_post(req: Request) -> tuple[ApiResponse, int]:
     p = Post(**postPublish.model_dump())
 
     try:
-        with db.session.begin():
-            db.session.add(p)
+        db.session.add(p)
     except Exception as e:
-        print(f'保存失败: {str(e)}')
+        return ApiResponse(code=ERROR_CODE,message=str(e)).model_dump(), 500
 
     resp = ApiResponse(
         code=SUCCESS_CODE,
         data=None
     )
     return resp.model_dump(), 200
+
+def get_posts_by_user_id_service(request: Request,user_id: int) -> tuple[ApiResponse, int]:
+    try:
+        page = max(int(request.args.get('page',1)),1)
+        page_size = min(max(int(request.args.get('page_size',10)),1),100)
+    except ValueError:
+        return ApiResponse(code=ERROR_CODE,message='参数错误').model_dump(),400
+
+    user = db.session.get(User,user_id)
+    total = len(user.posts)
+    stmt = (
+        select(Post)
+        .where(Post.user_id == user_id)
+        .order_by(Post.create_at.desc())
+        .offset((page-1)*page_size)
+        .limit(page_size)
+    )
+    data = db.session.execute(stmt).scalars().all()
+
+    select_result = [
+        PostResponse(
+            title=post.title,
+            content=post.content,
+            username=post.user.username,
+            avatar=post.user.avatar,
+            create_at=post.create_at
+        )
+        for post in data
+    ]
+
+    result = {
+        'result':select_result,
+        'total': total,
+        'page': page,
+        'page_size': page_size,
+        'total_pages': (total + page_size - 1) // page_size
+    }
+    return ApiResponse(code=SUCCESS_CODE,data=result).model_dump(),200
